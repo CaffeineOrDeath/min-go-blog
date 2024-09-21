41909044156d75af49d89b0299321bd2f2a11a5e
wokflow_gen <CaffeineOrDeath@users.noreply.github.com>
2024-09-21 11:07:05 +0000

    Commit from GitHub Actions (Go)

--- internal/views/components/logo_templ.go
+++ internal/views/components/logo_templ.go
@@ -36,7 +36,7 @@ func Logo(path string, alt string) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(path)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/components/logo.templ`, Line: 4, Col: 16}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/logo.templ`, Line: 4, Col: 16}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
@@ -49,7 +49,7 @@ func Logo(path string, alt string) templ.Component {
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(alt)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/components/logo.templ`, Line: 4, Col: 28}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/logo.templ`, Line: 4, Col: 28}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
